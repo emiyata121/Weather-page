da2079e85e43c8ad17531aa136706d6c78f0fae3
emiyata121 <59000047+emiyata121@users.noreply.github.com>
2022-05-08 14:44:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -50,12 +50,12 @@ function displayForecast(response) {
           width="42"
         />
         <div class="weather-forecast-temperatures">
-         <span class="weather-forecast-temperature-max"> ${Math.round(
-           forecastDay.temp.max
-         )}° | </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
+         <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min
-          )}° </span>
+          )}° | </span><span class="weather-forecast-temperature-max"> ${Math.round(
+           forecastDay.temp.max
+         )}° </span>
+          
         </div>
       </div>
   `;
